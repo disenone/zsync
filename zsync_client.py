223edd8390662305176e5be8db03da0654666587
disenone <disenonec@gmail.com>
2017-01-13 17:52:11 +0800

    add send file code

---
+++ zsync_client.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import zmq
+import os
+import time
+from threading import Thread
+from zhelpers import socket_set_hwm, zpipe
+from zsync_server import CHUNK_SIZE, PIPELINE, mypath
+
+ip = '192.168.11.249'
+ports = [13330, 13331, 13332]
+
+
+def recv_one():
+    return
+
+def client_thread(ctx, port):
+    dealer = ctx.socket(zmq.DEALER)
+    socket_set_hwm(dealer, PIPELINE)
+    dealer.connect("tcp://%s:%d" % (ip, port))
+
+    credit = PIPELINE   # Up to PIPELINE chunks in transit
+
+    total = 0           # Total bytes received
+    chunks = 0          # Total chunks received
+    offset = 0          # Offset of next chunk request
+
+    dealer.send_multipart([b'fetch'])
+
+    try:
+        fname = dealer.recv()
+    except zmq.ZMQError as e:
+        if e.errno == zmq.ETERM:
+            return   # shutting down, quit
+        else:
+            raise
+
+    outf = open(mypath + '/' + fname, 'w')
+
+    while True:
+        while credit:
+            # ask for next chunk
+            dealer.send_multipart([
+                b"fetch",
+                b"%i" % total,
+                b"%i" % CHUNK_SIZE,
+            ])
+
+            offset += CHUNK_SIZE
+            credit -= 1
+
+        try:
+            chunk = dealer.recv()
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                return   # shutting down, quit
+            else:
+                raise
+
+        outf.write(chunk)
+
+        chunks += 1
+        credit += 1
+        size = len(chunk)
+        total += size
+        if size < CHUNK_SIZE:
+            break   # Last chunk received; exit
+
+    print ("%i chunks received, %i bytes" % (chunks, total))
+    pipe.send(b"OK")
+    return
+
+if __name__ == '__main__':
+    begint = time.time()
+    ctx = zmq.Context()
+    clients = [Thread(target=client_thread, args=(ctx, port,)) for port in ports]
+    [client.start() for client in clients]
+    [client.join() for client in clients]
+    endt = time.time()
+    print 'finish: %ss' % (endt - begint)
--- zsync_server.py
+++ zsync_server.py
@@ -1,2 +1,86 @@
 # -*- coding: utf-8 -*-
 
+import zmq
+import os
+import time
+from threading import Thread
+from zhelpers import socket_set_hwm, zpipe
+from multiprocessing import Queue
+
+fileq = None
+mypath = 'sync_files'
+ports = [13330, 13331, 13332]
+CHUNK_SIZE = 250000
+PIPELINE = 10
+
+def send_one(socket):
+    if fileq.empty():
+        return False
+
+    try:
+        fname = fileq.get(True, 0.001)
+    except Queue.Empty, e:
+        return True
+
+    try:
+        msg = socket.recv_multipart()
+    except zmq.ZMQError as e:
+        if e.errno == zmq.ETERM:
+            return False   # shutting down, quit
+        else:
+            raise
+    identity, command = msg
+    assert command == b"fetch"
+    socket.send_multipart([identity, fname])
+
+    while True:
+        try:
+            msg = socket.recv_multipart()
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                return False   # shutting down, quit
+            else:
+                raise
+    
+        identity, command, offset_str, chunksz_str = msg
+        
+        assert command == b"fetch"
+        offset = int(offset_str)
+        chunksz = int(chunksz_str)
+
+        # Read chunk of data from file
+        file.seek(offset, os.SEEK_SET)
+        data = file.read(chunksz)
+
+        # Send resulting chunk to client
+        socket.send_multipart([identity, data])
+
+    return True
+
+def server_thread(ctx, port):
+    router = ctx.socket(zmq.ROUTER)
+    socket_set_hwm(router, PIPELINE)
+    router.bind("tcp://*:%d" % port)
+    
+    while True:
+        ret = send_one(router)
+        if not ret:
+            break
+
+    return
+
+if __name__ == '__main__':
+    begint = time.time()
+    onlyfiles = [os.path.join(mypath, f) for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) ]
+
+    fileq = Queue(len(onlyfiles))
+    for f in onlyfiles:
+        fileq.put_nowait(f)
+
+    ctx = zmq.Context()
+    servers = [Thread(target=server_thread, args=(ctx, port,)) for port in ports]
+    [server.start() for server in servers]
+    [server.join() for server in servers]
+    endt = time.time()
+
+    print 'finish: %s' % (endt - begint)
